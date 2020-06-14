fbc56e075842b4a96202efe68c96bd9256a6630a
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-14 21:37:24 +0200

    Working on windows mingw fix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@ if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
 cmake ^
-    -G "Visual Studio 15 2017 Win64" ^
+    -G "MinGW Makefiles" ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
