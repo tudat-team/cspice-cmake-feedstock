6f8642c7fa7d1304a62e2d5ba6777e419abc30cb
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-14 22:47:28 +0200

    Fixing win64 build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ if errorlevel 1 exit 1
 set PATH=%LIBRARY_PREFIX%\mingw-w64\bin\;%PATH%
 if errorlevel 1 exit 1
 cmake ^
-    -G "Unix Makefiles" ^
+    -G "NMake Makefile" ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
