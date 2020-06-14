9310020367d24b744ae6c1852bfe4c9b84676606
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-14 22:53:49 +0200

    Working on windows mingw fix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ if errorlevel 1 exit 1
 set PATH=%LIBRARY_PREFIX%\mingw-w64\bin\;%PATH%
 if errorlevel 1 exit 1
 cmake ^
-    -G "NMake Makefile" ^
+    -G "NMake Makefiles" ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
