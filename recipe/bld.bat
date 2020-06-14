76a8b8f0761241deb24fa3b0e0d8c53c738769b4
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-14 23:37:53 +0200

    Working on windows mingw fix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,14 +2,11 @@ mkdir build
 if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
-set PATH=%LIBRARY_PREFIX%\mingw-w64\bin\;%PATH%
-if errorlevel 1 exit 1
 cmake ^
-    -G "NMake Makefiles" ^
-    -DCMAKE_CXX_STANDARD=17 ^
+    -G "MinGW Makefiles" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_BUILD_TYPE=Debug ^
     -DCSPICE_BUILD_STATIC_LIBRARY=1 ^
     ..
 if errorlevel 1 exit 1
