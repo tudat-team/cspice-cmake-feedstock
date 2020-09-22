dfc1d8f071366fe342279fece50af1e5eb677ff0
ggarrett13 <g.h.garrett13@gmail.com>
2020-09-22 18:00:53 +0200

    Changed to clang (win)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,10 +3,9 @@ if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
 cmake ^
-    -G "MinGW Makefiles" ^
+    -G "NMake Makefiles" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DCMAKE_BUILD_TYPE=Debug ^
     -DCSPICE_BUILD_STATIC_LIBRARY=1 ^
     ..
 if errorlevel 1 exit 1
