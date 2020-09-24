d55e587dc0de346824b7d0b5beac551a3cae866e
ggarrett13 <g.h.garrett13@gmail.com>
2020-09-24 17:35:52 +0200

    Updated git hash

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ cmake ^
     -G "%CMAKE_GEN%" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DCSPICE_BUILD_STATIC_LIBRARY=0 ^
+    -DCSPICE_BUILD_STATIC_LIBRARY=1 ^
     ..
 if errorlevel 1 exit 1
 cmake --build . --config RelWithDebInfo --target install
