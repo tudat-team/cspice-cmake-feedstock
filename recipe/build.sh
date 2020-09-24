afc96c2acf297977e8a55921d1e6cd381e1b5235
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-09-24 16:20:02 +0200

    Added isatty fix (win)

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,10 @@ mkdir build
 cd build
 
 cmake \
-    -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     -DCMAKE_PREFIX_PATH="$PREFIX" \
-    -DCSPICE_BUILD_STATIC_LIBRARY=1 \
+    -DCSPICE_BUILD_STATIC_LIBRARY=0 \
     ..
 
 make -j2
